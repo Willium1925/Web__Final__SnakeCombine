56cf7af840916178ac347198916189544b006406
Willium1925 <d1397026@o365.fcu.edu.tw>
2025-06-09 15:18:52 +0800

    axios

--- app.js
+++ app.js
@@ -18,3 +18,25 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
+
+
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const port = 3000;
+
+// 中介層
+app.use(cors());
+app.use(bodyParser.json());
+
+// 靜態資料夾
+app.use(express.static('public'));
+
+// 分數 API
+const scoreRouter = require('./routes/scores');
+app.use('/scores', scoreRouter);
+
+// 啟動伺服器
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
